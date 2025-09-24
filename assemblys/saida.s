5b0a7b0a86380ff57b02c1bf62003a98c11bcdeb
jovilourenco <pessoal.jvls@gmail.com>
2025-09-23 22:34:18 -0300

    Atualizando cmd

--- assemblys/saida.s
+++ assemblys/saida.s
@@ -1,53 +1,90 @@
   .section .bss
   .lcomm a, 8
   .lcomm b, 8
+  .lcomm r, 8
 
 
   .section .text
   .globl _start
 
 _start:
-    mov $5, %rax
+    mov $18, %rax
     mov %rax, a
-    mov $5, %rax
+    mov $12, %rax
     mov %rax, b
+    mov $0, %rax
+    mov %rax, r
+    mov a, %rax
+    mov %rax, r
+Linicio1:
     mov b, %rax
     push %rax
-    mov a, %rax
-    pop %rbx
-    xor %rcx, %rcx
-    cmp %rbx, %rax
-    setl %cl
-    mov %rcx, %rax
-    cmp $0, %rax
-    jz Lfalso1
+    mov r, %rax
+    push %rax
     mov $1, %rax
-    jmp Lexit0
-    jmp Lfim2
-
-Lfalso1:
-    mov b, %rax
-    push %rax
-    mov a, %rax
+    pop %rbx
+    add %rbx, %rax
     pop %rbx
     xor %rcx, %rcx
     cmp %rbx, %rax
-    setz %cl
+    setg %cl
     mov %rcx, %rax
     cmp $0, %rax
-    jz Lfalso3
-    mov $2, %rax
-    jmp Lexit0
-    jmp Lfim4
-
-Lfalso3:
-    mov $3, %rax
-    jmp Lexit0
-
-Lfim4:
+    jz Lfim2
+    mov b, %rax
+    push %rax
+    mov r, %rax
+    pop %rbx
+    sub %rbx, %rax
+    mov %rax, r
+    jmp Linicio1
 
 Lfim2:
+Linicio3:
     mov $0, %rax
+    push %rax
+    mov r, %rax
+    pop %rbx
+    xor %rcx, %rcx
+    cmp %rbx, %rax
+    setg %cl
+    mov %rcx, %rax
+    cmp $0, %rax
+    jz Lfim4
+    mov b, %rax
+    mov %rax, a
+    mov r, %rax
+    mov %rax, b
+    mov a, %rax
+    mov %rax, r
+Linicio5:
+    mov b, %rax
+    push %rax
+    mov r, %rax
+    push %rax
+    mov $1, %rax
+    pop %rbx
+    add %rbx, %rax
+    pop %rbx
+    xor %rcx, %rcx
+    cmp %rbx, %rax
+    setg %cl
+    mov %rcx, %rax
+    cmp $0, %rax
+    jz Lfim6
+    mov b, %rax
+    push %rax
+    mov r, %rax
+    pop %rbx
+    sub %rbx, %rax
+    mov %rax, r
+    jmp Linicio5
+
+Lfim6:
+    jmp Linicio3
+
+Lfim4:
+    mov b, %rax
 Lexit0:
 
     call imprime_num
