d21fd871ae912b730040124f2c974967380e88f2
jovilourenco <pessoal.jvls@gmail.com>
2025-10-04 18:51:53 -0300

    Código apresentado

--- assemblys/saida.s
+++ assemblys/saida.s
@@ -1,35 +1,33 @@
   .section .text
-.globl inc
-inc:
+.globl maioridade
+maioridade:
     push %rbp
     mov %rsp, %rbp
     sub $8, %rsp
 
     mov $0, %rax
     mov %rax, -8(%rbp)
-Linicio2:
-    mov 16(%rbp), %rax
+    mov $18, %rax
     push %rax
-    mov -8(%rbp), %rax
+    mov 16(%rbp), %rax
     pop %rbx
     xor %rcx, %rcx
     cmp %rbx, %rax
-    setle %cl
+    setge %cl
     mov %rcx, %rax
     cmp $0, %rax
-    jz Lfim3
-    mov -8(%rbp), %rax
-    push %rax
+    jz Lfalso2
     mov $1, %rax
-    pop %rbx
-    add %rbx, %rax
     mov %rax, -8(%rbp)
-    jmp Linicio2
+    jmp Lfim3
+Lfalso2:
+    mov $0, %rax
+    mov %rax, -8(%rbp)
 Lfim3:
     mov -8(%rbp), %rax
-    jmp Lret_inc_1
+    jmp Lret_maioridade_1
 
-Lret_inc_1:
+Lret_maioridade_1:
     add $8, %rsp
     pop %rbp
     ret
@@ -42,9 +40,9 @@ _start:
     push %rbp
     mov %rsp, %rbp
 
-    mov $7, %rax
+    mov $20, %rax
     push %rax
-    call inc
+    call maioridade
     add $8, %rsp
 Lexit_main_0:
 
